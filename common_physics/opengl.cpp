a6fd422bd0ea3111aeb92bdb23d4e7b9b75973f7
gen BTC <genBTC@gmx.com>
2017-11-05 10:50:39 -0500

    Color rotation got messed up somewhere along the way.

--- common_physics/opengl.cpp
+++ common_physics/opengl.cpp
@@ -10,8 +10,6 @@
 #include <math.h>
 #include "utils.h"
 
-
-
 ///////////////////////////////////////////////////////////////////////////////
 void utSetOrthographicProjection(int scrW, int scrH) {
 
