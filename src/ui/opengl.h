9243d8651bc230635fbd53e3d348b35ebd61441a
genBTC <genBTC@gmx.com>
2023-10-22 14:12:24 -0400

    fixed unresolved symbol UTDrawText2D after inline works

--- src/ui/opengl.h
+++ src/ui/opengl.h
@@ -22,8 +22,12 @@ void utResetPerspectiveProjection();
 void utDrawText2D(float x, float y, void *font, const char *string);
 void utDrawText3D(float x, float y, float z, void *font, const char *string);
 // simplier version for default font...
-void utDrawText2D(float x, float y, const char *string);
-void utDrawText3D(float x, float y, float z, const char *string);
+inline void utDrawText2D(float x, float y, const char *string) {
+	utDrawText2D(x, y, GLUT_BITMAP_8_BY_13, string);
+}
+inline void utDrawText3D(float x, float y, float z, const char *string) {
+	utDrawText3D(x, y, z, GLUT_BITMAP_8_BY_13, string);
+}
 
 void utCalculateAndPrintFps(float x, float y);
 void utCalculateAndPrintAngles(float x, float y, double x1, double y1);
